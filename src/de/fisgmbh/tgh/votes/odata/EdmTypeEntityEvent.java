95039249f1f9059033df32870300bab7353b6c35
s.seufert <s.seufert@fis-gmbh.de>
2016-11-28 20:34:40 +0100

    BUG: pom.xml

--- src/de/fisgmbh/tgh/votes/odata/EdmTypeEntityEvent.java
+++ src/de/fisgmbh/tgh/votes/odata/EdmTypeEntityEvent.java
@@ -1,10 +1,17 @@
 package de.fisgmbh.tgh.votes.odata;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class EdmTypeEntityEvent extends EdmTypeEntityJPAStandard {
 
 	private static final String ET_NAME = "Event";
+	private static Map<String, String> customPropertyNames;
+
+	static {
+		customPropertyNames = new HashMap<>();
+		customPropertyNames.put("VoteDetails", "Votes");
+	}
 
 	public EdmTypeEntityEvent() {
 		super(ET_NAME);
@@ -12,7 +19,11 @@ public class EdmTypeEntityEvent extends EdmTypeEntityJPAStandard {
 
 	@Override
 	protected Map<String, String> getCustomPropertyNames() {
-		return null;
+		return customPropertyNames;
 	}
 
+	@Override
+	public String getCustomPropertyName(String originalPropertyName) {
+		return super.getCustomPropertyName(originalPropertyName);
+	}
 }
